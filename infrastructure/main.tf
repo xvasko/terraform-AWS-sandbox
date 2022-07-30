35dae650c84ccd4951eae8d5f191982047f1e638
Your Name <you@example.com>
2022-07-30 17:38:49 +1000

    create 3 subnets

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,3 +18,28 @@ resource "aws_vpc" "main" {
 
   enable_dns_hostnames = true
 }
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.main.id
+
+  // 256 available IPs
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-southeast-2a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = aws_vpc.main.id
+
+  // 256 available IPs
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "ap-southeast-2b"
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id = aws_vpc.main.id
+
+  // 256 available IPs
+  cidr_block        = "10.0.102.0/24"
+  availability_zone = "ap-southeast-2c"
+}

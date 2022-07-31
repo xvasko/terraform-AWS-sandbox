82902a46eb72bbed2f049875e9ee37bacb7913f4
Your Name <you@example.com>
2022-07-31 10:20:57 +1000

    add a tag to ec2 instance so it's targetable by AWS CodeDeploy

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -135,6 +135,10 @@ resource "aws_instance" "app_server" {
   subnet_id              = aws_subnet.public_subnet.id
   vpc_security_group_ids = [aws_security_group.web_security_group.id]
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+
+  tags = {
+    Name = "ServianWebAppInstance"
+  }
 }
 
 data "aws_iam_policy" "AmazonSSMManagedInstanceCore" {
